4f36cbf4bfd00d97f963e5500e875fb612dde888
gBritz <gbritzv@gmail.com>
2015-04-11 18:47:26 -0300

    feat: Classe que armazena erros de validação.

---
+++ HeroisDoCancer/HeroisDoCancer/Services/Exceptions/CamposInvalidosException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HeroisDoCancer.Services.Exceptions
+{
+    public class CamposInvalidosException : Exception
+    {
+        private readonly IDictionary<String, String[]> campos;
+
+        public CamposInvalidosException(IDictionary<String, String[]> campos)
+        {
+            if (campos == null)
+                throw new ArgumentNullException("campos");
+
+            this.campos = campos;
+        }
+
+        public IDictionary<String, String[]> Campos
+        {
+            get { return campos; }
+        }
+    }
+}
