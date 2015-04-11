4d61baa0ea708d11b1414fa68bcfc57b03958d9a
gBritz <gbritzv@gmail.com>
2015-04-11 20:53:16 -0300

    feat: Classe para tipar dados da sessão do back-end.

---
+++ HeroisDoCancer/HeroisDoCancer/WebUtils/SessionWrapper.cs
@@ -0,0 +1,43 @@
+﻿using HeroisDoCancer.Models;
+using System;
+using System.Linq;
+using System.Web;
+
+namespace HeroisDoCancer.WebUtils
+{
+    public class SessionWrapper
+    {
+        private readonly HttpSessionStateBase session;
+
+        public SessionWrapper(HttpSessionStateBase session)
+        {
+            if (session == null)
+                throw new ArgumentNullException("session");
+
+            this.session = session;
+        }
+
+        public Voluntario Voluntario
+        {
+            get { return (Voluntario)session["Voluntario"]; }
+            set { SetSafe("Voluntario", value); }
+        }
+
+        public void Close()
+        {
+            session.Clear();
+            session.Abandon();
+        }
+
+        protected virtual void SetSafe(String key, Object value)
+        {
+            if (value == null)
+            {
+                if (session.Keys.OfType<String>().Any(k => k == key))
+                    session.Remove(key);
+            }
+            else
+                session.Add(key, value);
+        }
+    }
+}
