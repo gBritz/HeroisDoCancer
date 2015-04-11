c1a931c2a8484740f9dd8f3d1aaced1a406aeadd
benguimaraes1995 <benguima@gmail.com>
2015-04-11 18:16:04 -0300

    - Contato - Modal login

---
+++ HeroisDoCancer/HeroisDoCancer/Controllers/GaleriaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HeroisDoCancer.Controllers
+{
+    public class GaleriaController : Controller
+    {
+        //
+        // GET: /Galeria/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
