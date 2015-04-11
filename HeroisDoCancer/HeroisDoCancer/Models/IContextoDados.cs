5f6c0a0e01d1e647be974f8c83844905fef385eb
gBritz <gbritzv@gmail.com>
2015-04-11 17:47:16 -0300

    fix: Desfazer rollback

---
+++ HeroisDoCancer/HeroisDoCancer/Models/IContextoDados.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace HeroisDoCancer.Models
+{
+    public interface IContextoDados
+    {
+        ICollection<Voluntario> Voluntarios { get; }
+
+        void Adiciona<TModel>(TModel model) where TModel : class;
+
+        void Remove<TModel>(TModel model) where TModel : class;
+
+        void Publica();
+
+        void Limpa();
+    }
+}
---
+++ HeroisDoCancer/HeroisDoCancer/Services/VoluntarioService.cs
@@ -0,0 +1,44 @@
+﻿using HeroisDoCancer.Models;
+using System;
+using System.Linq;
+
+namespace HeroisDoCancer.Services
+{
+    public class VoluntarioService
+    {
+        private IContextoDados contexto;
+
+        public VoluntarioService(IContextoDados contexto)
+        {
+            if (contexto == null)
+                throw new ArgumentNullException("contexto");
+
+            this.contexto = contexto;
+        }
+
+        public void Cadastra(Voluntario voluntario)
+        {
+            if (voluntario == null)
+                throw new ArgumentNullException("voluntario");
+
+            if (!ValidoCadastro(voluntario))
+                throw new Exception("Dados incompletos.");
+
+            if (ExistePeloNome(voluntario))
+                throw new Exception("Nome de voluntario já existe.");
+
+            this.contexto.Adiciona(voluntario);
+            this.contexto.Publica();
+        }
+
+        public virtual Boolean ExistePeloNome(Voluntario voluntario)
+        {
+            return this.contexto.Voluntarios.Any(v => v.Nome.Equals(voluntario.Nome, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        protected virtual Boolean ValidoCadastro(Voluntario voluntario)
+        {
+            throw new NotImplementedException("TODO");
+        }
+    }
+}
