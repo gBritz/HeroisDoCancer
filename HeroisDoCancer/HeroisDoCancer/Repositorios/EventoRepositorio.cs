71444a0bda2a31a4eb91015cb48c99a8b60cb240
gBritz <gbritzv@gmail.com>
2015-04-12 02:18:50 -0300

    feat: Repositório para consultar de evento.

---
+++ HeroisDoCancer/HeroisDoCancer/Repositorios/EventoRepositorio.cs
@@ -0,0 +1,29 @@
+﻿using HeroisDoCancer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HeroisDoCancer.Repositorios
+{
+    public class EventoRepositorio
+    {
+        private IContextoDados contexto;
+
+        public EventoRepositorio(IContextoDados contexto)
+        {
+            if (contexto == null)
+                throw new ArgumentNullException("contexto");
+
+            this.contexto = contexto;
+        }
+
+        public Evento[] ObterTodosConfirmados()
+        {
+            return this.contexto.Eventos
+                .Where(e => e.TipoSituacao == TipoSituacaoEnum.Confirmado)
+                .OrderByDescending(e => e.CriadoEm)
+                .ToArray();
+        }
+    }
+}
